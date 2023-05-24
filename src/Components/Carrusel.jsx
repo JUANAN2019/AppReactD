b898b871cdc453e43ce1250dc1fa44716df9146e
Juanan2019 <garciagalindo@telefonica.net>
2023-05-24 11:49:28 +0200

    primer

---
+++ src/Components/Carrusel.jsx
@@ -0,0 +1,46 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+export default function Carrusel() {
+  return (
+    <Carousel slide={false}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://www.nosolorol.com/img/cms/2023/chapters/WEB-Vampire-Chapters.jpg?text=First slide&bg=373940"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://www.nosolorol.com/img/cms/2023/El-Libro-del-Olvido---RN389---Wraith-20A-banner-web-02_2.jpg?text=Second slide&bg=282c34"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://www.nosolorol.com/img/cms/2023/Banner-web-Umbra---La-Sombra-de-Terciopelo---RN398.jpg?text=Third slide&bg=20232a"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
